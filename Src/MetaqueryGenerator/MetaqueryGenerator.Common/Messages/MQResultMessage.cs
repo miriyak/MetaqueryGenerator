f1b13600de90f6d5e25eee21f4c46285a73fe721
Miri Yakobi <miriyakobi@gmail.com>
2017-12-26 22:18:11 +0200

    add MQ messages

---
+++ Src/MetaqueryGenerator/MetaqueryGenerator.Common/Messages/MQResultMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MetaqueryGenerator.Common
+{
+    public interface IMQResultMessage
+    {
+    }
+    public class MQResultMessage : IMQResultMessage
+    {
+        public int ID;
+        public bool Result;
+    }
+    public class MQAssignmentResultMessage : IMQResultMessage
+    {
+        public int ID;
+        public float SupportValue { get; set; }
+        public float ConfidenceValue  { get; set; }
+        public string Assignment { get; set; }
+
+    }
+}
---
+++ Src/MetaqueryGenerator/MetaqueryGenerator.Common/Messages/SendMQMessage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MetaqueryGenerator.Common
+{
+    public class SendMQMessage
+    {
+        public int ID { get; set; }
+        public float SupportThreshold { get; set; }
+        public float ConfidenceThreshold { get; set; }
+        public int[] Head { get; set; }
+        public List<List<int>> Body { get; set; }
+        
+        /*
+         o	המידע שנשלח ל – MQ Solver
+	ID – מספר פנימי במערכת MQ Generator
+	Support (ערך הסף של support ) במבנה 0.xx 
+	Confidence (ערך הסף של Confidence) במבנה 0.xx 
+	Head (מערך של מספרי המשתנים המרכיבים את ראש השאילתה) 
+{1,3}
+	Body (מערך דו מימדי של מספרי המשתנים המרכיבים את כל היחסים בגוף השאילתה) במבנה: 
+{{1,2,3},{2,3},{3}}
+
+ */
+    }
+}
